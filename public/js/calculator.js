cf40cbd7e4701f76086f6a7627f8ff893755509a
Anthony <martab7.am@gmail.com>
2016-06-19 16:34:13 -0500

    Updates files with minor corrections

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,17 +1,17 @@
 'use strict'
 // var result = document.getElementById('result');
-var buttons = document.getElementsByClassName('button');
-var left = document.getElementById('left');
-var middle = document.getElementById('middle');
-var right = document.getElementById('right');
-var inputs = document.getElementsByTagName('input');
-var clear = document.getElementById('clear');
-var operators = document.getElementsByClassName('button-operator');
-var completed = false;
-var squareRoot = document.getElementById('square');
-var plusMinus = document.getElementById('plus-minus');
-var percentage = document.getElementById('percentage');
-   
+var buttons = document.getElementsByClassName('button')
+var left = document.getElementById('left')
+var middle = document.getElementById('middle')
+var right = document.getElementById('right')
+var inputs = document.getElementsByTagName('input')
+var clear = document.getElementById('clear')
+var operators = document.getElementsByClassName('button-operator')
+var completed = false
+var squareRoot = document.getElementById('square')
+var plusMinus = document.getElementById('plus-minus')
+var percentage = document.getElementById('percentage')
+
 left.value = '';
 middle.value = '';
 right.value = '';
@@ -19,7 +19,7 @@ right.value = '';
 function click() {
   var selection = this.getAttribute('data-value');
 
-  
+
   if (middle.value === '' && selection && selection != '=') {
     left.value += selection;
   } else if ((left.value && middle.value) && selection != '+' && selection != '=') {
@@ -30,7 +30,7 @@ function click() {
     // }
   } else if(selection === '=') {
     operate();
-  } 
+  }
 
   function operate() {
     var left_number = parseFloat(left.value);
@@ -61,7 +61,7 @@ function operatorSymbol() {
   if(selection === '+' || selection === '-' || selection === 'x' || selection === '/' || selection === '√' || selection === '^' || selection === '±' || selection === '%') {
     middle.value = '';
     middle.value = selection;
-  }; 
+  };
 };
 
 function clearInputs() {
@@ -71,7 +71,7 @@ function clearInputs() {
 }
 
 function squareNumber() {
-  
+
 }
 
 for(var i = 0; i < buttons.length; i++) {
@@ -83,5 +83,3 @@ for(var i = 0; i < operators.length; i++) {
 }
 
 clear.addEventListener('click', clearInputs, false);
-
-
